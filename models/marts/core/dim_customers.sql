cc8b9c57158f3e38ceffad7a7a8f54444d54f924
Pedro Navarro <chispeter98@gmail.com>
2023-11-24 11:50:53 +0000

    dim and ints

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -2,39 +2,47 @@ WITH customers AS (
     SELECT * 
     FROM {{ ref('stg_sql_server_dbo__customers') }}
     ),
-
+    
 addresses AS (
     SELECT * 
     FROM {{ ref('stg_sql_server_dbo__addresses') }}
-    ),
+),
 
-int_customer_orders__grouped AS (
+int_customer_orders__joined AS (
     SELECT *
-    FROM {{ ref('int_customer_orders__grouped') }}
+    FROM {{ ref('int_customer_orders__joined') }}
     ),
 
 dim_customers AS (
     SELECT
-        -- customer_addresses?? int
+        -- CUSTOMER_ADDRESSES?? -> INT
+        -- customers
         customers.customer_id AS customer_id,
         customers.customer_first_name AS customer_first_name,
         customers.customer_last_name AS customer_last_name,
         customers.customer_phone_number AS customer_phone_number,
         customers.customer_email AS customer_email,
+        -- dates
+        cast(customers.customer_created_at_utc as date) AS customer_creation_date,
+        cast(customers.customer_updated_at_utc as date) AS customer_update_date,
+        -- addresses
         addresses.address_street_number AS customer_street_number,
         addresses.address_street_name AS customer_street_name,
         addresses.address_state_name AS customer_state_name,
         addresses.address_zipcode AS customer_zipcode,
         addresses.address_country_name AS customer_country_name,
-        -- customer_orders (falta añadir del int)
-        customer_orders.first_order_date AS customer_first_order_date,
-        customer_orders.last_order_date AS customer_last_order_date,
-        coalesce(customer_orders.number_of_orders, 0) AS customer_number_of_orders,
-        customers.customer_created_at_utc AS customer_created_at_utc,
-        customers.customer_updated_at_utc AS customer_updated_at_utc
+        -- customer_orders_joined
+        customer_orders.oldest_order_date AS oldest_order_date,
+        customer_orders.most_recent_order_date AS most_recent_order_date,
+        customer_orders.cheapest_order_cost_in_usd AS cheapest_order_cost_in_usd,
+        customer_orders.most_expensive_order_cost_in_usd AS most_expensive_order_cost_in_usd,
+        customer_orders.average_order_cost_in_usd AS average_order_cost_in_usd,
+        customer_orders.total_amount_spent_in_usd AS total_amount_spent_in_usd,
+        customer_orders.number_of_total_orders AS number_of_total_orders,
+        customer_orders.customer_value_in_usd AS customer_value_in_usd
     FROM customers
-    JOIN addresses ON customers.customer_address_id = addresses.address_id
-    JOIN int_customer_orders__grouped customer_orders ON customers.customer_id = customer_orders.customer_id
+    LEFT JOIN addresses ON customers.customer_address_id = addresses.address_id
+    LEFT JOIN int_customer_orders__joined AS customer_orders ON customers.customer_id = customer_orders.customer_id
     )
 
 SELECT * FROM dim_customers
--- models/marts/core/intermediate/int_customer_orders__grouped.sql
+++ models/marts/core/intermediate/int_customer_orders__grouped.sql
@@ -1,24 +1,24 @@
 WITH orders AS (
     SELECT * 
     FROM {{ ref('stg_sql_server_dbo__orders') }}
-    ),
+),
 
 int_customer_orders__grouped AS (
-    SELECT orders.order_customer_id AS customer_id,
+    SELECT order_customer_id AS order_customer_id,
             -- order dates
-            cast(min(orders.order_created_at_utc) as date) AS oldest_order_date,
-            cast(max(orders.order_created_at_utc) as date) AS most_recent_order_date,
+            cast(min(order_created_at_utc) as date) AS oldest_order_date,
+            cast(max(order_created_at_utc) as date) AS most_recent_order_date,
             -- order cost aggregations
-            cast(min(orders.order_cost_in_usd) as number(38,2)) AS cheapest_order_cost_in_usd,
-            cast(max(orders.order_cost_in_usd) as number(38,2)) AS most_expensive_order_cost_in_usd,
-            cast(avg(orders.order_cost_in_usd) as number(38,2)) AS average_order_cost_in_usd,
-            cast(sum(orders.order_cost_in_usd) as number(38,2)) AS total_amount_spent_in_usd,
+            cast(min(order_cost_in_usd) as number(38,2)) AS cheapest_order_cost_in_usd,
+            cast(max(order_cost_in_usd) as number(38,2)) AS most_expensive_order_cost_in_usd,
+            cast(avg(order_cost_in_usd) as number(38,2)) AS average_order_cost_in_usd,
+            cast(sum(order_cost_in_usd) as number(38,2)) AS total_amount_spent_in_usd,
             -- number of orders
-            cast(count(orders.order_customer_id) as number(38,0)) AS number_of_total_orders,
+            cast(count(order_customer_id) as number(38,0)) AS number_of_total_orders,
             -- customer_value = average_order_cost_in_usd * number_of_orders
-            cast((avg(orders.order_cost_in_usd) *  count(orders.order_customer_id)) as number(38,2)) AS customer_value_in_usd
+            cast((avg(order_cost_in_usd) *  count(order_customer_id)) as number(38,2)) AS customer_value_in_usd
     FROM orders
-    GROUP BY orders.order_customer_id
+    GROUP BY order_customer_id
     )
 
 SELECT * FROM int_customer_orders__grouped
