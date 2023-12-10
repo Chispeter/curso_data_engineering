b25149de21db8b20b461a7cf8d4a19013574f476
Pedro Navarro <chispeter98@gmail.com>
2023-12-10 02:54:57 +0000

    fct_events finished

--- models/marts/core/fct_events.sql
+++ models/marts/core/fct_events.sql
@@ -1,19 +1,35 @@
 WITH stg_events AS (
-    SELECT * 
-    FROM {{ ref('stg_sql_server_dbo__events') }}
-),
-
-fct_events AS (
     SELECT
         event_id,
+        cast(event_created_at_utc as date) AS event_creation_date,
+        cast(event_created_at_utc as time) AS event_creation_time,
         customer_id,
         session_id,
         product_id,
         order_id,
-        event_created_at_utc,
         page_url,
         event_type
-    FROM stg_events
-    )
+    FROM {{ ref('stg_sql_server_dbo__events') }}
+),
+
+stg_dates AS (
+    SELECT * 
+    FROM {{ ref('stg_staging__dates') }}
+),
+
+fct_events AS (
+    SELECT
+        e.event_id              AS event_id,
+        e.customer_id           AS customer_id,
+        e.session_id            AS session_id,
+        e.product_id            AS product_id,
+        e.order_id              AS order_id,
+        d.date_id               AS event_creation_date_id,
+        e.event_creation_time   AS event_creation_time,
+        e.page_url              AS page_url,
+        e.event_type            AS event_type
+    FROM stg_events AS e
+    LEFT JOIN stg_dates AS d ON e.event_creation_date = d.date_day
+)
 
 SELECT * FROM fct_events
--- models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
@@ -13,7 +13,7 @@ stg_sql_server_dbo__events AS (
         cast(session_id as varchar(50))                                                                             AS session_id,
         cast(decode(product_id, '', {{ dbt_utils.generate_surrogate_key(['null']) }}, product_id) as varchar(50))   AS product_id,
         cast(decode(order_id, '', {{ dbt_utils.generate_surrogate_key(['null']) }}, order_id) as varchar(50))       AS order_id,
-        cast(created_at as timestamp_tz(9))                                                                         AS event_created_at_utc,
+        cast(created_at as timestamp_ntz(9))                                                                        AS event_created_at_utc,
         cast(page_url as varchar(200))                                                                              AS page_url,
         cast(event_type as varchar(50))                                                                             AS event_type,
         cast(coalesce(_fivetran_deleted, false) as boolean)                                                         AS was_this_row_deleted,
