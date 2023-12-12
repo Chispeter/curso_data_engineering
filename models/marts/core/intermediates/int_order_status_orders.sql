fc7223bdb4f62f21a6142638db2c9d3da69cd2d2
Pedro Navarro <chispeter98@gmail.com>
2023-12-12 17:08:49 +0000

    fix

---
+++ models/marts/core/intermediates/int_order_status_orders.sql
@@ -0,0 +1,14 @@
+WITH distinct_order_status AS (
+    SELECT
+        distinct(distinct_order_status) AS order_status
+    FROM {{ ref('stg_sql_server_dbo__orders') }}
+),
+
+int_order_status_orders AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['order_status']) }}   AS order_status_id,
+        order_status                                               AS order_status
+    FROM distinct_order_status
+)
+
+SELECT * FROM int_order_status_orders
